be03c20698d65c5cf5d517220f6c038f6ecd0199
lizardon <chia.qien.don@dhs.sg>
2021-01-26 00:10:17 +0800

    Initial Commit

---
+++ swiftui-reference-library/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-reference-library
+//
+//  Created by Don Chia on 26/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-reference-library/swiftui_reference_libraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_reference_libraryApp.swift
+//  swiftui-reference-library
+//
+//  Created by Don Chia on 26/1/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_reference_libraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

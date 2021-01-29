b5fb2ab8b756d031d19657936575fda75c97adc7
lizardon <chia.qien.don@dhs.sg>
2021-01-29 11:52:45 +0800

    added list element

---
+++ swiftui-reference-library/Basic Layout/list.swift
@@ -0,0 +1,27 @@
+//
+//  list.swift
+//  swiftui-reference-library
+//
+//  Created by Don Chia on 29/1/21.
+//
+
+import SwiftUI
+
+struct list: View {
+    var body: some View {
+        NavigationView {
+            List {
+                Text("Item 1")
+                Text("Item 2")
+                Text("Item 3")
+            }
+            .navigationBarTitle("List")
+        }
+    }
+}
+
+struct list_Previews: PreviewProvider {
+    static var previews: some View {
+        list()
+    }
+}
